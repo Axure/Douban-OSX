0edb789ac349c12e379878666081c0600041822a
Axure <axurez@vip.qq.com>
2015-09-26 20:34:09 +0800

    Create project in Xcode 7.

---
+++ Douban@OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Douban@OSX
+//
+//  Created by 郑虎 on 2015-09-26.
+//  Copyright © 2015年 Axurez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
